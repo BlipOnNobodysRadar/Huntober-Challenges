7735dfd2bcb9dce46c0fe035755e5c457e81f607
Ryan Lowe <somebody480@gmail.com>
2022-10-19 18:10:59 -0400

    challenge4 getAverage complete

---
+++ Challenge4 ArrayAverage.js
@@ -0,0 +1,18 @@
+/*
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+getAverage([2,2,2,2]),2)
+getAverage([1,2,3,4,5,]),3);
+getAverage([1,1,1,1,1,1,1,2]),1)
+*/
+
+function getAverage(arr) {
+  return arr.reduce((sum, grade) => grade + sum, 0) / arr.length;
+}
+getAverage([2, 2, 2, 2]);
+getAverage([1, 2, 3, 4, 5]);
+getAverage([1, 1, 1, 1, 1, 1, 1, 2]);
