faf7a5e10ae282f1eb5843f911328b8fd4b98ad3
Ryan Lowe <somebody480@gmail.com>
2022-10-24 18:03:52 -0400

    challenge6 complete

---
+++ Challenge6 evenArr.js
@@ -0,0 +1,16 @@
+/*
+Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+For example:
+
+([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+*/
+function evenArr(arr, n) {
+  const evens = arr.filter((num) => num % 2 === 0);
+  return evens.slice(evens.length - n);
+}
+console.log(evenArr([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+console.log(evenArr([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2));
+console.log(evenArr([6, -25, 3, 7, 5, 5, 7, -3, 23], 1));
